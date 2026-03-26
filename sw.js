f29ddbc2b21e6d9790131058e9b50396a9fc6419
pravinzende1989 <70840144+pravinzende1989@users.noreply.github.com>
2026-03-26 21:39:54 +0530

    Add service worker for offline caching

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'pz-seo-cache-v1';
+const OFFLINE_URL = '/?source=pwa';
+
+// Files to cache for offline access
+const urlsToCache = [
+  '/',
+  OFFLINE_URL,
+  'ttps://www.pravinzende.co.in/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request) || caches.match(OFFLINE_URL);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
